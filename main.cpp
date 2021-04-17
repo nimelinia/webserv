5250e8544aeb01d6daa53ab7d45df63a43669f1d
Streetmentioner Copycat <scopycat@ox-h5.msk.21-school.ru>
2021-04-17 21:15:06 +0300

    make includes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
-//
-// Created by Natalia Troyanovskaya on 02.04.2021.
-//
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <exception>
+#include <ctime>
+#include <cstring>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <csignal>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <cstdio>
+#include <cerrno>
+#include <ctime>
+#include <sys/select.h>
+#include <sys/types.h>
 
-int main()
+
+int main(int argc, char **argv)
 {
-	return (0);
+	if (argc != 2)
+	{
+		std::cout << "There is no config for server!" << std::endl;
+		return (1);
+	}
+	SOCK_ addr;
+	
+	return 0;
 }
