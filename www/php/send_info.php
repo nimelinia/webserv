9880cb445bfe2da9ce1ab5cba7c08f524304889a
mjuli <mjuli@student.21-school.ru>
2021-07-06 15:01:11 +0300

    Added files to check php-cgi with GET and POST requests

--- www/php/send_info.php
+++ www/php/send_info.php
@@ -1,6 +1,16 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST["name"];
+    $address = $_POST["address"];
+} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $name = $_GET["name"];
+    $address = $_GET["address"];
+}
+?>
+
 <html>
-<title>Thank You Page</title>
+<title>Thank You from <?php echo $_SERVER['REQUEST_METHOD']; ?></title>
 <h1>Thank you for filling out my form!</h1>
-Thank you, <b><?php echo $_POST["name"]; ?></b>, for filling out my form!
-I will mail information to <b><?php echo $_POST["address"]; ?></b> right away.
+Thank you, <b><?php echo $name; ?></b>, for filling out my form!
+I will mail information to <b><?php echo $address; ?></b> right away.
 </html>
